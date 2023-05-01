9f5fd9036f094becd2b8d7dd31088cb743d5aa01
Jason Closterman <devjclosterman@gmail.com>
2023-05-01 06:21:51 -0700

    adding darkMode

---
+++ React-Next-Vue/src/components/hooks/useDarkMode.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+     try {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+     } catch(error) {
+        console.log(error)
+        return initialValue;     
+     }
+    });
+    const setValue = (value) => {
+        try
+    }
+}

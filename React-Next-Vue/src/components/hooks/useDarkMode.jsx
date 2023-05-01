482d9ee25e537137dae69edbe2cd76c1c2495072
Jason Closterman <devjclosterman@gmail.com>
2023-05-01 06:31:23 -0700

    finished darkMode hook

--- React-Next-Vue/src/components/hooks/useDarkMode.jsx
+++ React-Next-Vue/src/components/hooks/useDarkMode.jsx
@@ -11,6 +11,28 @@ const useLocalStorage = (key, initialValue) => {
      }
     });
     const setValue = (value) => {
-        try
-    }
+        try {
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        } catch(error) {
+            console.log(error);
+        }
+    };
+    return [storedValue, setValue];
+};
+
+const useDarkMode = () => {
+    const [enabled, setEnabled] = useLocalStorage('dark-theme');
+    const isEnabled = typeof enabledState === 'undefined' && enabled;
+
+    useEffect(() => {
+        const className = 'dark';
+        const bodyClass = window.document.body.classList;
+
+        isEnabled ? bodyClass.add(className) : bodyClass.remove(className);
+    }, [enabled, isEnabled]);
+    return [enabled, setEnabled];
 }
+
+export default useDarkMode;
